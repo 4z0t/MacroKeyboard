b84f8e44f45548f37d8e1027d581b2976e532c7b
4z0t <gastec139@gmail.com>
2022-09-27 21:34:01 +0300

    images

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,19 @@
 
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <Keyboard.h>
 #include <Keypad.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET -1     
+#define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 const byte ROWS = 3; // four rows
 const byte COLS = 3; // four columns
